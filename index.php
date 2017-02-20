3e3c40542bdc8b1a0091865d8d9a6b4b84266fc3
girijavk <girija.vk1@gmail.com>
2017-02-20 12:21:06 +0530

    starter code

--- index.php
+++ index.php
@@ -1,1 +1,110 @@
-<? phpinfo() ?>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Microsoft Cloud Connected App Development Workshop</title>
+	<style type="text/css">
+		body{
+			text-align: center;
+			background-color: #ebebeb;
+		}
+		img{
+			width: 300px;
+		}
+		h1{
+			padding: 10px;
+		}
+		h4{
+			color: red;
+		}
+		.dark{
+			background-color: #3498db;
+			color: #FFF;
+		}
+		.light{
+			background-color: #9bcff2;
+		}
+		table{
+			margin: auto;
+			display: inline-table;
+			/*border: 1px solid #9bcff2;*/
+		}
+		td{
+			padding: 3px;
+			text-align: center;
+			border: 1px solid #9bcff2;
+		}
+	</style>
+</head>
+<body>
+	<img src="images/xamarin.png">
+	<h1>Microsoft Cloud Connected App Development</h1>
+	<h3>in association with Department of Information Science and Engineering, Ramaiah Institute of technology</h3>
+	<br>
+	<br>
+	<h3>Date: 4 Feb, 2017</h3>
+	<h3>Venue: Intel COE, R&D block, MSRIT</h3>
+	<h4>Registrations are closed</h4>
+	<!--<a href="https://goo.gl/forms/Q6Uci48nfdMHPzc52">Click here to Apply</a>-->
+	
+	<h3>Agenda</h3>
+	<table>
+		<tr class="dark">
+			<td>Time</td>
+			<td>Topic</td>
+		</tr>
+		<tr>
+			<td>9:00 AM - 10:00 AM</td>
+			<td>C# and Xamarin basics</td>
+		</tr>
+		<tr class="light"> 
+			<td>10:00 AM - 10:15 AM</td>
+			<td>Break</td>
+		</tr>
+		<tr>
+			<td>11:15 AM - 12:00 PM</td>
+			<td>Building Portable Class Libraries in Xamarin.</td>
+		</tr>
+		<tr class="light">
+			<td>12:00 PM - 1:00 PM</td>
+			<td>Lunch</td>
+		</tr>
+		<tr>
+			<td>1:00 PM - 2:00 PM</td>
+			<td>Xamarin Forms development - Android, iOS and Windows App ready!</td>
+		</tr>
+		<tr>
+			<td>2:00 PM - 3:00 PM</td>
+			<td>Introduction to Azure.</td>
+		</tr>
+		<tr>
+			<td>3:00 PM - 4:00 PM</td>
+			<td>Azure Mobile Apps - Connect to a SQL database. Create a Node.js backend.</td>
+		</tr>
+		<tr class="light">
+			<td>4:00 PM - 4:15 PM</td>
+			<td>Break</td>
+		</tr>
+		<tr>
+			<td>4:15 PM - 5:15 PM</td>
+			<td>Connect to Azure Mobile App with Xamarin client app (Android, iOS and Windows).</td>
+		</tr>
+		<tr>
+			<td>5:15 pm - 5:45 PM</td>
+			<td>Push notifications; Adding Facebook, Twitter authentication to your Android, iOS and Windows apps.</td>
+		</tr>
+		<tr>
+			<td>5:45 PM - 6:00 PM</td>
+			<td>Wrap Up</td>
+		</tr>
+	</table>
+	<br>
+	<br>
+	<h3>Instructions to install Xamarin and Online Resources</h3>
+	<a href="docs/Xamarin Installation and Online Resources for Pre Learning.pdf">Windows</a>
+	<br>
+	<a href="docs/Xamarin Installation and Online Resources for Pre Learning for macOS.pdf">macOS</a>
+	<br>
+	<br>
+	<h3>Contact <a href="mailto:xamarinmsrit@gmail.com">xamarinmsrit@gmail.com</a> for queries</h3>
+</body>
+</html>
